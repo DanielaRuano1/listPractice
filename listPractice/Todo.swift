bd1f5ac5b97693ea99f25c53ea0db4a0af65db8e
Daniela Ruano <ruthdanielaruano@gmail.com>
2022-06-28 09:36:41 -0600

    Initial Commit

---
+++ listPractice/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  listPractice
+//
+//  Created by Daniela Ruano on 28/06/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "String"
+    var important = false
+}
---
+++ listPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listPractice
+//
+//  Created by Daniela Ruano on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
